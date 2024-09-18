4fafaddad53b2f29788e47dd1fb58ee7529d0045
Isabella <igodom04@gmail.com>
2024-09-18 17:50:34 -0500

    made splash screen load image

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,33 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
-
-
     public static void main(String[] args) {
-    
+      
+        //Create main screen
         JFrame frame = new JFrame("Photon Laser Tag System");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
-        frame.setLocationRelativeTo(null); 
+        frame.setLocationRelativeTo(null);
 
-        // Show splash screen
+        //Show splash screen
         Splash.showSplashScreen(frame);
 
-        // Show home screen
-        JLabel label = new JLabel("Photon Laser Tag System", SwingConstants.CENTER);
-        frame.add(label);
-        frame.setVisible(true);
-    }
+        //Remove splash screen photo after 5 secs
+        Timer timer = new Timer(5000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Label main screen
+                JLabel label = new JLabel("Photon Laser Tag System", SwingConstants.CENTER);
+                frame.add(label);
+                
+                //Show main screen
+                frame.setVisible(true);
+            }
+        });
+        timer.setRepeats(false);
+        timer.start();
+    
+}
 }
--- src/Splash.java
+++ src/Splash.java
@@ -2,28 +2,60 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.IOException;
 
 public class Splash {
-    public static void showSplashScreen(JFrame mainFrame) {
-       
+    private static BufferedImage splashImage;
 
-        //Create new window
+
+    //Displays splash screen
+    public static void showSplashScreen(JFrame mainFrame) {
+        //Load splash image
+        imageLoad();
+
+        //Create window
         JWindow window = new JWindow();
-        window.setSize(300, 200);
-    
-        //Create and show label
-        JLabel label = new JLabel(new ImageIcon("src/assets/images/splash.jpg"));
-        window.getContentPane().add(label, BorderLayout.CENTER);
+        window.setSize(800, 600);
+        window.setLocationRelativeTo(null);
+
+        //Create panel to draw image
+        JPanel imagePanel = new JPanel() {
+         
+            //draw splash image
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                if (splashImage != null) {
+                    g.drawImage(splashImage, 0, 0, this.getWidth(), this.getHeight(), this);
+                }
+           }
+        };
+
+        //Add image panel to window
+        window.getContentPane().add(imagePanel, BorderLayout.CENTER);
         window.setVisible(true);
 
         //Show splash screen for five seconds
-        Timer timer = new Timer(5000, new ActionListener() {
+          Timer timer = new Timer(5000, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                window.dispose(); 
+                window.dispose();
             }
         });
         timer.setRepeats(false);
         timer.start();
+    }   
+
+    //Load Splash Image
+    private static void imageLoad() {
+        try {
+            splashImage = ImageIO.read(Splash.class.getResourceAsStream("/assets/images/splash.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-}
+} 
+
+
+
