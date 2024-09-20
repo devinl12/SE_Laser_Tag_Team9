14d1279090fc5c2ec189b2ddd08f3ec4b07e95f8
emmaherman <112578644+emmaherman@users.noreply.github.com>
2024-09-20 15:56:10 -0500

    Adding things in

--- src/build.bash
+++ src/build.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -u -e  
 # Compile the Java files
-javac Main.java Splash.java
+javac -cp "lib/postgresql-42.7.4.jar" *.java
 # Run the program
-java -cp lib/postgres-42.7.4.jar Main
+java -cp "lib/postgresql-42.7.4.jar" Main
