59c4bf069315bb472208772122d8cb6fb717974b
emmaherman <112578644+emmaherman@users.noreply.github.com>
2024-09-21 17:34:22 -0500

    Update Connection.java

--- src/Connection.java
+++ src/Connection.java
@@ -1,13 +1,11 @@
 import java.sql.*;
-
-public class Connection {
+public class PSQLConn {
 	public static void main(String[] args) {
 		String db_url = "jdbc:postgresql://localhost:5432/photon";
 		String user = "student";
 		String pass = "student";
 		String query = "select * from players;";
 		try (Connection connection = DriverManager.getConnection(db_url, user, pass);
-
 			Statement stmt = connection.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
 		) {
