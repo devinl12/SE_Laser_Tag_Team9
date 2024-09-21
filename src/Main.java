2f16d0874f37f41dacc7cc023ff6db8ed964b135
emmaherman <112578644+emmaherman@users.noreply.github.com>
2024-09-21 14:36:56 -0500

    Adding Stuff

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,24 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
-      
-        //Create main screen
+        // Create main screen
         JFrame frame = new JFrame("Photon Laser Tag System");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.setLocationRelativeTo(null);
 
-        //Show splash screen
+        // Show splash screen
         Splash.showSplashScreen(frame);
+        System.out.print("Showing Spalsh Screen")
 
-        //Remove splash screen photo after 5 secs
-        Timer timer = new Timer(5000, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //Label main screen
-                JLabel label = new JLabel("Photon Laser Tag System", SwingConstants.CENTER);
-                frame.add(label);
-                
-                //Show main screen
-                frame.setVisible(true);
-            }
+        // Timer to remove splash screen after 5 secs
+        Timer timer = new Timer(5000, e -> {
+            // Remove splash screen and show the player screen
+            PlayerScreen playerScreen = new PlayerScreen(frame);
+            playerScreen.showPlayerScreen();
         });
         timer.setRepeats(false);
         timer.start();
-    
-}
+    }
 }
