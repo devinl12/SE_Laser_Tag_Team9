e7fc45b60751edb86ea0c79ff53b198372819b1a
emmaherman <112578644+emmaherman@users.noreply.github.com>
2024-09-21 14:44:00 -0500

    Updating build.bash file

--- src/build.bash
+++ src/build.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -u -e  
 # Compile the Java files
-javac -cp "lib/postgresql-42.7.4.jar" *.java
+javac -cp "lib/postgresql-42.7.4.jar" Main.java  playerScreen.java PSQLConn.java Splash.java PlayerService.java
 # Run the program
 java -cp "lib/postgresql-42.7.4.jar" Main
