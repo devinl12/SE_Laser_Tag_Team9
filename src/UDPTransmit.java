298fc1fdf2e5b497acc375531810b93ac3e6bc45
gaberivera7 <gaberivera779@gmail.com>
2024-09-21 18:41:12 -0600

    idk

--- src/UDPTransmit.java
+++ src/UDPTransmit.java
@@ -10,13 +10,13 @@ public class UDPTransmit {
 
             DatagramSocket socket = new DatagramSocket(); 
             //using 255.255.255.255 broadcast address to send to all devices on the same local network
-            InetAddress multicastAddress = InetAddress.getByName("255.255.255.255");
+            InetAddress broadcastAddress = InetAddress.getByName("127.0.0.1");
             
             String message = String.valueOf(equipmentId);
             byte[] packetBytes = message.getBytes();
             
             // creating packet containing the equipment ID and sending over the UDP socket to the multicast address
-            DatagramPacket packet = new DatagramPacket(packetBytes, packetBytes.length, multicastAddress, 7500);
+            DatagramPacket packet = new DatagramPacket(packetBytes, packetBytes.length, broadcastAddress, 7500);
             socket.send(packet);
 
             //for debugging
