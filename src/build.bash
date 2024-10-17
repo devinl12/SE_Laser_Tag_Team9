ec62fe90bb278cd5410ff850d948031cd8ef1c9f
student account <student@debian-BULLSEYE-live-builder-AMD64>
2024-10-17 16:57:08 -0500

    added method to initiate countdown to countdown class to minimize code in Main file

--- src/build.bash
+++ src/build.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -u -e  
 # Compile the Java files
-javac Main.java  PlayerScreen.java Splash.java PlayerService.java UDPTransmit.java UDPReceive.java
+javac Main.java  PlayerScreen.java Splash.java PlayerService.java UDPTransmit.java UDPReceive.java ImageCountdown.java
 # Run the program
 java -cp "../lib/postgresql-42.7.4.jar":. Main
